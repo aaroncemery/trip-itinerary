e321dd9412e86456305724926a5856ad05b9ec05
Aaron Emery <aaron.emery@sanity.io>
2025-07-08 16:48:37 -0700

    feat: homepage

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: {},
+  },
+  images: {
+    minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/**`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
